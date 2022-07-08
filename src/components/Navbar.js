69e19a55e8c48daec069307120c32c98c2b5bd6e
Dominic Maglione <dcmaglione@gmail.com>
2022-07-07 21:59:43 -0400

    minor styling, not yet responsive, links still bugged

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-import { FiSun, FiMoon } from 'react-icons/fi';
+import { FiSun, FiMoon, FiMenu } from 'react-icons/fi';
 import DarkMode from '../hooks/DarkMode';
 
 const Navbar = () => {
   return (
-    <div className="fixed flex flex-row justify-between h-16 w-screen text-3xl text-secondaryTextLight dark:text-secondaryTextDark">
+    <div className="py-12 px-28 z-10 fixed flex flex-row justify-between w-screen text-3xl text-secondaryTextLight dark:text-secondaryTextDark">
       <Link
         activeClass="active"
         to="root"
         spy={true}
         smooth={true}
         duration={600}
-        className="hover:text-hoverTextLight dark:hover:text-hoverTextDark transition-all cursor-pointer"
+        className="hover:text-hoverTextLight dark:hover:text-hoverTextDark transition-all cursor-pointer font-extrabold"
       >
-        dcmag
+        DCMAG
       </Link>
-      <ThemeIcon />
+      <div className="flex flex-row gap-4">
+        <ThemeIcon />
+        <FiMenu />
+      </div>
     </div>
   );
 };
