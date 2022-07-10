c4bc891411eb7581139d704e6d458f3df0dcdb8b
Dominic Maglione <dcmaglione@gmail.com>
2022-07-10 18:28:11 -0400

    fixed icon, navbar

--- src/components/DropMenu.js
+++ src/components/DropMenu.js
@@ -24,7 +24,7 @@ const DropMenu = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-center xs:origin-top-right absolute -right-11 xs:right-0 mt-1 w-max shadow-md border-2 border-primaryBgDark dark:border-primaryBgLight bg-secondaryBgDark dark:bg-secondaryBgLight text-2xl font-mono font-semibold">
+        <Menu.Items className="origin-top-right absolute right-0 mt-4 w-max shadow-md border-2 border-primaryBgDark dark:border-primaryBgLight bg-secondaryBgDark dark:bg-secondaryBgLight text-2xl font-mono font-semibold">
           <div className="p-1/4">
             <Menu.Item>
               {({ active }) => (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import DropMenu from './DropMenu';
 
 const Navbar = () => {
   return (
-    <div className="pt-6 pb-2 xs:pb-2 xs:pt-8 px-6 lg:px-16 z-10 fixed left-1/2 -translate-x-1/2 flex flex-col xs:flex-row items-center xs:items-start gap-2 xs:gap-0 justify-between w-[94vw] text-3xl text-secondaryTextLight dark:text-secondaryTextDark bg-primaryBgLight dark:bg-primaryBgDark max-w-screen-2xl">
+    <div className="pt-6 pb-2 px-6 lg:px-16 z-10 fixed left-1/2 -translate-x-1/2 flex flex-row items-center xs:items-start gap-2 xs:gap-0 justify-between w-[94vw] text-3xl text-secondaryTextLight dark:text-secondaryTextDark bg-primaryBgLight dark:bg-primaryBgDark max-w-screen-2xl border-b-4 border-primaryBgDark dark:border-primaryBgLight">
       <a
         href="/"
         className="hover:text-hoverTextLight dark:hover:text-hoverTextDark transition-all cursor-pointer font-extrabold"
