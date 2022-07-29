89d7bb19fd11d4335173156f7c1e1b0572332a7b
Dominic Maglione <dcmaglione@gmail.com>
2022-07-29 10:35:37 -0400

    Minor changes to font and footer padding

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="relative mb-24 xs:my-10  flex flex-col text-center gap-2 xs:gap-0 xs:flex-row xs:text-justify justify-between">
+    <div className="relative my-10  flex flex-col text-center gap-2 xs:gap-0 xs:flex-row xs:text-justify justify-between">
       <div className="text-xl">© 2022 Dominic Maglione</div>
       <a
         className="text-xl font-mono hover:text-accent hover:font-bold transition-all"
